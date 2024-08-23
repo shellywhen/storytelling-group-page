3d7bceec0dec36d480b8c629e15a29b50babf507
shellywhen <xieliwenhan@yahoo.com>
2024-08-23 20:44:32 +0800

    update short paper

--- _site/assets/js/filter.js
+++ _site/assets/js/filter.js
@@ -1,4 +1,4 @@
-let status = {
+let btnStatus = {
     'all': true,
     'storytelling': false,
     'design': false,
@@ -14,31 +14,37 @@ d3.select('.all-button').classed('active', true)
 
 function filterPub(btn) {
     const value = btn.getAttribute("data-value")
-    status[value] = !status[value]
-    const sum = Object.values(status).reduce((a,b) => a || b, false)
-    if(!sum) {
-        status['all'] = true
+    btnStatus[value] = !btnStatus[value]
+    if (value === 'all') {
+        btnStatus = {
+            'all': true,
+            'storytelling': false,
+            'design':false,
+            'authoring': false
+        }
+    } else {
+        if (btnStatus[value] === false) {
+            btnStatus = {
+                'all': true,
+                'storytelling': false,
+                'design': false,
+                'authoring': false
+            }
+        } else {
+            btnStatus = {
+                'all': false,
+                'storytelling': false,
+                'design': false,
+                'authoring': false
+        }
+        btnStatus[value] = true
     }
-    if (value =='all' && status['all']) {
-        status['storytelling'] = false
-        status['design'] = false
-        status['authoring'] = false
-    } else if (value!= 'all' && sum) {
-        status['all'] = false
-    } else if (!sum) {
-        status['all'] = true
-    }
+}
 
-    if (status['storytelling'] && status['design'] && status['authoring']) {
-        status['all'] = true 
-        status['storytelling'] = false
-        status['design'] = false
-        status['authoring'] = false
-    }
     const tags = []
     d3.select('.filter-panel').selectAll('div').each(function(){
         const value = this.getAttribute("data-value")
-        if (status[value]) {
+        if (btnStatus[value]) {
             d3.select(this).classed('active', true)
             tags.push(...tagMapping[value])
         } else {
